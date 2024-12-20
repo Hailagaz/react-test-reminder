65a31dcf3c500ee19654d78c4c96a662e48bb666
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2024-12-20 11:37:13 +0200

    fix main section align

--- my-react-app/src/components/MainSection.js
+++ my-react-app/src/components/MainSection.js
@@ -3,20 +3,20 @@ import { Container, Box, Typography } from '@mui/material';
 
 const MainSection = () => {
 	return (
-		<Container>
-			<Box id="home" sx={{ padding: 4 }}>
+		<Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+			<Box id="home" sx={{ padding: 4, textAlign: 'center' }}>
 				<Typography variant="h2">Welcome to My React App</Typography>
 				<Typography>This is a simple application to help you remember React.js concepts.</Typography>
 			</Box>
-			<Box id="about" sx={{ padding: 4 }}>
+			<Box id="about" sx={{ padding: 4, textAlign: 'center' }}>
 				<Typography variant="h4">About Us</Typography>
 				<Typography>We are a team of developers passionate about React.js.</Typography>
 			</Box>
-			<Box id="services" sx={{ padding: 4 }}>
+			<Box id="services" sx={{ padding: 4, textAlign: 'center' }}>
 				<Typography variant="h4">Our Services</Typography>
 				<Typography>We offer a wide range of services to help you build your React applications.</Typography>
 			</Box>
-			<Box id="contact" sx={{ padding: 4 }}>
+			<Box id="contact" sx={{ padding: 4, textAlign: 'center' }}>
 				<Typography variant="h4">Contact Us</Typography>
 				<Typography>Feel free to reach out to us for any inquiries.</Typography>
 			</Box>
